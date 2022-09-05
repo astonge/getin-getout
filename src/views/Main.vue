db3bb27dcdb8a269128738c82ef3b3fa07c0acbe
astonge <astonge@gmail.com>
2022-09-05 10:05:21 +0000

    moved logged users email display to sidebar

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -17,7 +17,6 @@
         
                 <div class="navbar-center text-center grid flex">
                     <a class="btn btn-ghost normal-case text-xl">Get in and Get Out</a>
-                    {{ firebase.auth().currentUser.email }}
                 </div>
                 <div class="navbar-end">
                     <div v-if="addMode">
@@ -90,7 +89,8 @@
         </div> 
         <div class="drawer-side">
             <label for="my-drawer-3" class="drawer-overlay"></label>
-            <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+            <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 gap-2">
+                <li class="bg-gray-700 font-bold rounded rounded-2xl p-4 text-center">{{ firebase.auth().currentUser.email }}</li>
                 <li><button type="button">Settings</button></li>
                 <li><button type="button" @click="logout">Logout</button></li>
             </ul>
