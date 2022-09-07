35b0308bd19f73c29daa0a5b1af475210efa543e
astonge <astonge@gmail.com>
2022-09-07 02:39:53 +0000

    update add label to button; added disabled when addMode = true

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -78,13 +78,13 @@
             </div>
 
             <div class="btm-nav p-2">
-                <label for="my-modal-4" class="btn modal-button bg-blue-600" @click="add">
-                <span class="text-4xl">
-                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z" />
-                    </svg>
-                </span>
-                </label>
+                <button :disabled="addMode" class="btn modal-button bg-blue-600" @click="add">
+                    <span class="text-4xl">
+                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z" />
+                        </svg>
+                    </span>
+                </button>
             </div>
         </div> 
         <div class="drawer-side">
